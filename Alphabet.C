8dc3757d5e4745a2b255ef82f8bd6422cea05204
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-02 14:10:49 +0530

    Alphabet.C

---
+++ Alphabet.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("Enter the character");
+scanf("%d",&a);
+if(a>='a'&&a<='z'||a>='A'&&a<='z')
+print("It is an Alphabet");
+else
+printf("It is Not an Alphabet");
+getch();
+return 0;
+}
